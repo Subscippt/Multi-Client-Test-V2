247ee391b79c3c77118f738de7e4d2e669039778
Leo - Home <18U-LMahoney@CambridgeAST.org.uk>
2022-02-08 23:51:33 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime;
+using System.Net;
+using System.Net.Sockets;
+namespace Multi_Client_Server
+{
+    class Program
+    {
+        static Listener l;
+        static List<Socket> sockets;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("started");
+            l = new Listener(8);
+            l.SocketAccepted += new Listener.SocketAcceptedHandler(l_SocketAccepted);
+            l.Start();
+            Console.Read();
+        }
+        static void l_SocketAccepted(System.Net.Sockets.Socket e)
+        {
+            Console.WriteLine("new connection " + e.RemoteEndPoint + " at " + DateTime.Now);
+            sockets.Add(e);
+        }
+    }
+
+}
